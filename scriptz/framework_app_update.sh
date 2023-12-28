b35d5928813137c534e599cdef65b066bbc72949
tank <tank@certifiedcoderz.com>
2023-12-27 20:50:04 -0500

    good

--- scriptz/framework_app_update.sh
+++ scriptz/framework_app_update.sh
@@ -19,24 +19,31 @@ read folder_id
 
 # test start #
 : '
-project_id=19
-app_type='cms'
-folder_id='cms'
+project_id=197
+app_type='mobile'
+folder_id='mobile'
 '
 # test end #
 
 G_BIZ_APP_DIR=${G_PROJECT_FOLDER}${project_id}/${folder_id}
 #go dir
 cd ${G_BIZ_APP_DIR}/
+
+source .biz9_config.sh
+app_title=${APP_TITLE}
+app_title_id=${APP_TITLE_ID}
+app_version=${APP_VERSION}
 # mk backup
 rm -rf .biz9_update_bk
 mkdir .biz9_update_bk
 
 if [ "${app_type}" = "mobile" ]; then
 FRAMEWORK_SRC_PROJECT=${G_PROJECT_SRC_FOLDER}${BIZ9_MOBILE_TITLE,,}/src/${G_BRANCH_DIR}/
+framework_title=${BIZ9_MOBILE_TITLE}
+framework_version=${BIZ9_MOBILE_VERSION}
 #source
 source ${FRAMEWORK_SRC_PROJECT}.biz9_config.sh
-   #bk configs
+  #bk configs
     cp -rf .biz9_config.sh .biz9_update_bk/
     cp -rf www/scripts/biz9-mobile/scriptz/config.js .biz9_update_bk/
     #copy src to project dir
@@ -57,11 +64,11 @@ source ${FRAMEWORK_SRC_PROJECT}.biz9_config.sh
     #update versionz
     sed -i "s/BIZ9_MOBILE_VERSION=.*/BIZ9_MOBILE_VERSION='${BIZ9_MOBILE_VERSION}';/" ${G_BIZ_APP_DIR}/www/scripts/biz9-mobile/scriptz/config.js
     sed -i "s/BIZ9_MOBILE_VERSION=.*/BIZ9_MOBILE_VERSION='${BIZ9_MOBILE_VERSION}';/" ${G_BIZ_APP_DIR}/.biz9_config.sh
-    echo "Project Update Complete"
-    print_result="BiZ9 Framework Mobile Version ${BIZ9_MOBILE_VERSION}"
     fi
 if [ "${app_type}" = "service" ]; then
     FRAMEWORK_SRC_PROJECT=${G_PROJECT_SRC_FOLDER}${BIZ9_SERVICE_TITLE,,}/src/${G_BRANCH_DIR}/
+    framework_title=${BIZ9_SERVICE_TITLE}
+    framework_version=${BIZ9_SERVICE_VERSION}
     #source
     source ${FRAMEWORK_SRC_PROJECT}.biz9_config.sh
     #bk configs
@@ -80,10 +87,11 @@ if [ "${app_type}" = "service" ]; then
     #update versionz
     sed -i "s/BIZ9_SERVICE_VERSION=.*/BIZ9_SERVICE_VERSION='${BIZ9_SERVICE_VERSION}';/" ${G_BIZ_APP_DIR}/.biz9_config.sh
     sed -i "s/BIZ9_SERVICE_VERSION=.*/BIZ9_SERVICE_VERSION='${BIZ9_SERVICE_VERSION}';/" ${G_BIZ_APP_DIR}/app.js
-    print_result="${BIZ9_SERVICE_TITLE} Version: ${BIZ9_SERVICE_VERSION}"
 fi
 if [ "${app_type}" = "cms" ]; then
     FRAMEWORK_SRC_PROJECT=${G_PROJECT_SRC_FOLDER}${BIZ9_CMS_TITLE,,}/src/${G_BRANCH_DIR}/
+    framework_title=${BIZ9_CMS_TITLE}
+    framework_version=${BIZ9_CMS_VERSION}
     #source
     source ${FRAMEWORK_SRC_PROJECT}.biz9_config.sh
     #bk configs
@@ -105,6 +113,8 @@ if [ "${app_type}" = "cms" ]; then
 fi
 if [ "${app_type}" = "server" ]; then
     FRAMEWORK_SRC_PROJECT=${G_PROJECT_SRC_FOLDER}${BIZ9_SERVER_TITLE,,}/src/${G_BRANCH_DIR}/
+    framework_title=${BIZ9_SERVER_TITLE}
+    framework_version=${BIZ9_SERVER_VERSION}
     #source
     source ${FRAMEWORK_SRC_PROJECT}.biz9_config.sh
     #bk configs
@@ -115,60 +125,15 @@ if [ "${app_type}" = "server" ]; then
     cp -rf  .biz9_update_bk/.biz9_config.sh ${G_BIZ_APP_DIR}/
     #update versionz
     sed -i "s/BIZ9_SERVER_VERSION=.*/BIZ9_SERVER_VERSION='${BIZ9_SERVER_VERSION}';/" ${G_BIZ_APP_DIR}/.biz9_config.sh
-    print_result="${BIZ9_SERVER_TITLE} Version: ${BIZ9_SERVER_VERSION}"
-fi
-if [ "${app_type}" = "vendor" ]; then
-    FRAMEWORK_SRC_PROJECT=${G_PROJECT_SRC_FOLDER}${BIZ9_VENDOR_TITLE,,}/src/${G_BRANCH_DIR}/
-    #source
-    source ${FRAMEWORK_SRC_PROJECT}.biz9_config.sh
-    #bk configs
-    cp -rf .biz9_config.sh .biz9_update_bk/
-    cp -rf app.js .biz9_update_bk/
-    #copy src to project dir
-    cp -rf ${FRAMEWORK_SRC_PROJECT}bin ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}node_modules ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}package.json ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}public ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}routes ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}scriptz ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}views ${G_BIZ_APP_DIR}/
-    #mv configs
-    cp -rf  .biz9_update_bk/.biz9_config.sh ${G_BIZ_APP_DIR}/
-    #update versionz
-    sed -i "s/BIZ9_VENDOR_VERSION=.*/BIZ9_VENDOR_VERSION='${BIZ9_VENDOR_VERSION}';/" ${G_BIZ_APP_DIR}/.biz9_config.sh
-    sed -i "s/BIZ9_VENDOR_VERSION=.*/BIZ9_VENDOR_VERSION='${BIZ9_VENDOR_VERSION}';/" ${G_BIZ_APP_DIR}/app.js
-    print_result="${BIZ9_VENDOR_TITLE} Version: ${BIZ9_VENDOR_VERSION}"
-fi
-
-if [ "${app_type}" = "vendor-payment" ]; then
-    FRAMEWORK_SRC_PROJECT=${G_PROJECT_SRC_FOLDER}${BIZ9_VENDOR_PAYMENT_TITLE,,}/src/${G_BRANCH_DIR}/
-    #source
-    source ${FRAMEWORK_SRC_PROJECT}.biz9_config.sh
-    #bk configs
-    cp -rf .biz9_config.sh .biz9_update_bk/
-    cp -rf app.js .biz9_update_bk/
-    #copy src to project dir
-    cp -rf ${FRAMEWORK_SRC_PROJECT}bin ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}node_modules ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}package.json ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}public ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}routes ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}scriptz ${G_BIZ_APP_DIR}/
-    cp -rf ${FRAMEWORK_SRC_PROJECT}views ${G_BIZ_APP_DIR}/
-    #mv configs
-    cp -rf  .biz9_update_bk/.biz9_config.sh ${G_BIZ_APP_DIR}/
-    #update versionz
-    sed -i "s/BIZ9_VENDOR_PAYMENT_VERSION=.*/BIZ9_VENDOR_PAYMENT_VERSION='${BIZ9_VENDOR_PAYMENT_VERSION}';/" ${G_BIZ_APP_DIR}/.biz9_config.sh
-    sed -i "s/BIZ9_VENDOR_PAYMENT_VERSION=.*/BIZ9_VENDOR_PAYMENT_VERSION='${BIZ9_VENDOR_PAYMENT_VERSION}';/" ${G_BIZ_APP_DIR}/app.js
-    print_result="${BIZ9_VENDOR_PAYMENT_TITLE} Version: ${BIZ9_VENDOR_PAYMENT_VERSION}"
 fi
 echo "----------------------------------"
-echo ${print_result}
+echo "Framework Product: ${framework_title}"
+echo "Framework Version: ${framework_version}"
 echo "Project-ID: ${project_id}"
 echo "App Type: ${app_type}"
-echo "App Title: ${APP_TITLE}"
-echo "App-Title-ID: ${APP_TITLE_ID}"
-echo "APP Version: ${APP_VERSION}"
+echo "App Title: ${app_title}"
+echo "App-Title-ID: ${app_title_id}"
+echo "APP Version: ${app_version}"
 echo "Directory: ${folder_id}"
 echo "Done!"
 echo "----------------------------------"
